0480a637152622394b97dc50179e17a1939016ea
chico ferreira <36338391+chicoferreira@users.noreply.github.com>
2024-05-16 14:25:49 +0100

    Estrutura ex 1.3

---
+++ ex1/app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+var plantasRouter = require('./routes/plantas');
+app.use('/plantas', plantasRouter);
+
+module.exports = app;
