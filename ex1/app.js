5d8bb20f5e30194d1d23c39c792c8e341a4424b1
chico ferreira <36338391+chicoferreira@users.noreply.github.com>
2024-05-31 12:52:00 +0100

    fix: docker with different name fix: errors not handled

--- ex1/app.js
+++ ex1/app.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-mongoose.connect('mongodb://ew-teste-mongodb-1/contratos');
+mongoose.connect('mongodb://engweb-teste-mongodb-1/contratos');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Erro de conexão com o banco de dados:'));
 db.once('open', function () {
--- ex2/routes/index.js
+++ ex2/routes/index.js
@@ -2,35 +2,55 @@ var express = require('express');
 var router = express.Router();
 var axios = require('axios');
 
-const api_dados = 'http://ew-teste-backend-1:16000';
+const api_dados = 'http://engweb-teste-backend-1:16000';
 
 router.get('/', async (req, res) => {
-  let contratosReq = await axios.get(`${api_dados}/contratos`);
-  let contratos = contratosReq.data;
-  res.render('index', { contratos: contratos });
+  axios.get(`${api_dados}/contratos`)
+      .then(response => {
+        let contratos = response.data;
+        res.render('index', {contratos: contratos});
+      })
+      .catch(_error => {
+        res.status(500).send('Erro ao obter contratos');
+      });
 });
 
 router.get('/entidades/:nipc', async (req, res) => {
-  let contratosReq = await axios.get(`${api_dados}/contratos?entidade=${req.params.nipc}`);
-  let contratos = contratosReq.data;
-  if (!contratos || contratos.length === 0) {
-    res.status(404).send('Entidade não encontrada');
-    return;
-  }
+  axios.get(`${api_dados}/contratos?entidade=${req.params.nipc}`)
+      .then(response => {
+        let contratos = response.data;
+        if (!contratos || contratos.length === 0) {
+          res.status(404).send('Entidade não encontrada');
+          return;
+        }
 
-  let entidade = contratos[0];
-  let entidade_nipc = entidade.NIPC_entidade_comunicante;
-  let entidade_nome = entidade.entidade_comunicante;
+        let entidade = contratos[0];
+        let entidade_nipc = entidade.NIPC_entidade_comunicante;
+        let entidade_nome = entidade.entidade_comunicante;
 
-  let preco = contratos.reduce((acc, contrato) => acc + contrato.precoContratual, 0);
+        let preco = contratos.reduce((acc, contrato) => acc + contrato.precoContratual, 0);
 
-  res.render('entidade', { contratos: contratos, entidade_nipc: entidade_nipc, entidade_nome: entidade_nome, preco: preco });
+        res.render('entidade', {
+          contratos: contratos,
+          entidade_nipc: entidade_nipc,
+          entidade_nome: entidade_nome,
+          preco: preco
+        });
+      })
+      .catch(_error => {
+        res.status(500).send('Erro ao obter contratos');
+      });
 });
 
 router.get('/:id', async (req, res) => {
-  let contratoReq = await axios.get(`${api_dados}/contratos/${req.params.id}`);
-  let contrato = contratoReq.data;
-  res.render('contrato', { contrato: contrato });
+  axios.get(`${api_dados}/contratos/${req.params.id}`)
+      .then(response => {
+        let contrato = response.data;
+        res.render('contrato', {contrato: contrato});
+      })
+      .catch(_error => {
+        res.status(500).send('Erro ao obter contrato');
+      });
 });
 
 module.exports = router;
